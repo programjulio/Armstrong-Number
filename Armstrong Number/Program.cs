d098f3b3707ec336db28cfac9acbee68216efda0
programjulio <programjulio@gmail.com>
2023-04-16 10:28:33 -0300

    commit do código

---
+++ Armstrong Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Armstrong_Number
+{
+    class ArmstrongNumber
+    {
+        static void Main(string[] args)
+        {
+            int num, temp, sum = 0, rem;
+            Console.WriteLine("Escolha um número : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            temp = num; 
+            while(num>0)
+            {
+                rem = num % 10;
+                sum = sum + rem * rem * rem;
+                num = num / 10;
+            }
+            
+            if(temp==sum)
+            {
+                Console.WriteLine("Número Narcisista.");
+            }
+            else
+            {
+                Console.WriteLine("Não é Narcisista.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
